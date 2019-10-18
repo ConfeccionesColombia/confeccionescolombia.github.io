35e43cb05b643b94930bd69a81b73d279a0fa8d5
rodrigo debian <correo.rodrigo@gmail.com>
2019-10-18 14:52:07 -0500

    descripciones

--- js/categorias.js
+++ js/categorias.js
@@ -2,52 +2,28 @@
 
 const Categories = [
     {
-	code: "BOL",
-	title: "Bolsos",
-	desc: "Bolsos de mujer en diferentes estilos y colores. Materiales duraderos y lavables.",
-	imgpath: "img/categorias/alforjas/002_resized.jpg"
-    },
-    {
-	code: "ALF",
-	title: "Alforjas",
-	desc: "Alforjas para motos, con reflejantes, varios colores, duraderas y resistentes.",
+	code: "alforjas",
+	title: "Alfojrjas",
+	desc: "Alforjas pra moto resistentes y durables. Variedad de colores en reflejantes.",
 	imgpath: "img/categorias/alforjas/003_resized.jpg"
     },
     {
-	code: "ALF",
-	title: "Alforjas",
-	desc: "Alforjas para motos, con reflejantes, varios colores, duraderas y resistentes.",
-	imgpath: "img/categorias/alforjas/003_resized.jpg"
+	code: "mochilas",
+	title: "Mochilas",
+	desc: "Mochilas en telas lavables y coloers tierra.",
+	imgpath: "img/categorias/mochilas/004_resized.jpg"
     },
     {
-	code: "ALF",
-	title: "Alforjas",
-	desc: "Alforjas para motos, con reflejantes, varios colores, duraderas y resistentes.",
-	imgpath: "img/categorias/alforjas/003_resized.jpg"
-    },    
-    {
-	code: "BOL",
-	title: "Bolsos",
-	desc: "Bolsos de mujer en diferentes estilos y colores. Materiales duraderos y lavables.",
-	imgpath: "img/categorias/alforjas/002_resized.jpg"
+	code: "demano",
+	title: "Bolsos de Mano",
+	desc: "Complementa tu vestuario con estos bonitos bolsos de mano.",
+	imgpath: "img/categorias/demano/001_resized.jpg"
     },
     {
-	code: "ALF",
-	title: "Alforjas",
-	desc: "Alforjas para motos, con reflejantes, varios colores, duraderas y resistentes.",
-	imgpath: "img/categorias/alforjas/003_resized.jpg"
-    },
-    {
-	code: "ALF",
-	title: "Alforjas",
-	desc: "Alforjas para motos, con reflejantes, varios colores, duraderas y resistentes.",
-	imgpath: "img/categorias/alforjas/003_resized.jpg"
-    },
-    {
-	code: "ALF",
-	title: "Alforjas",
-	desc: "Alforjas para motos, con reflejantes, varios colores, duraderas y resistentes.",
-	imgpath: "img/categorias/alforjas/003_resized.jpg"
+	code: "lapiceras",
+	title: "Lapiceras",
+	desc: "Prácticas para guardar todo tipo de cositas.",
+	imgpath: "img/categorias/lapiceras/001_resized.jpg"
     }
 ];
 
