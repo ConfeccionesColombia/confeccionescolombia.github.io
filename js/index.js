e22483fdc6ecd2c92639e121fe17b28a938cd2d1
rodrigo debian <correo.rodrigo@gmail.com>
2019-10-18 16:37:50 -0500

    refactoring

--- js/index.js
+++ js/index.js
@@ -5,37 +5,13 @@ class MainModel {
 	this.items = ko.observableArray(items);
 	this.splitted = ko.pureComputed(() => {
 	    let list = this.items();
-	    let split = [];
-	    for (let i = 0; i < list.length; i++) { 
-		if (i === 0 || i % 3 === 0) {
-		    split.push([]);
-		}
-		let arr = split[split.length -1];
-		arr.push(list[i]);
-	    }
-	    
-	    
-	    return split;
+	    return Services.split(list);
 	}, this);
     }
 }
 
-class Binder {
-    bind(model, selector) {
-	let jqObj = $(selector);
-	if (jqObj.length === 0) {
-	    return;
-	}
 
-	let domObj = jqObj[0];
-	ko.cleanNode(domObj);
-	ko.applyBindings(model, domObj);
-    }
-}
-
-
-$(function(){
-    let binder = new Binder();
+$(function(){    
     let main = new MainModel(Categories);
-    binder.bind(main, "main:first");
+    Services.bind(main, "main:first");
 });
