6cc3bc3f90ba09ae690d37c346889682885a3159
rodrigo debian <correo.rodrigo@gmail.com>
2019-10-18 10:53:09 -0500

    resized

--- js/categorias.js
+++ js/categorias.js
@@ -5,13 +5,13 @@ const Categories = [
 	code: "BOL",
 	title: "Bolsos",
 	desc: "Bolsos de mujer en diferentes estilos y colores. Materiales duraderos y lavables.",
-	imgpath: "img/categorias/alforja.jpeg"
+	imgpath: "img/categorias/alforjas/002_resized.jpg"
     },
     {
 	code: "ALF",
 	title: "Alforjas",
 	desc: "Alforjas para motos, con reflejantes, varios colores, duraderas y resistentes.",
-	imgpath: "img/categorias/alforja.jpeg"
+	imgpath: "img/categorias/alforjas/003_resized.jpg"
     }
 ];
 
